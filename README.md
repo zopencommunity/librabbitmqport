d2bb441494b63a073ddd1d2f34f822818c17bc94
Mike Grossmann <mike.grossmann@me.com>
2025-02-12 02:09:38 -0600

    Project created.

---
+++ README.md
@@ -0,0 +1,5 @@
+[![Automatic version updates](https://github.com/zopencommunity/librabbitmqport/actions/workflows/bump.yml/badge.svg)](https://github.com/zopencommunity/librabbitmqport/actions/workflows/bump.yml)
+
+# librabbitmq
+
+RabbitMQ C AMQP client library
---
+++ cicd-stable.groovy
@@ -0,0 +1,14 @@
+node('linux')
+{
+  stage ('Poll') {
+    checkout([
+      $class: 'GitSCM',
+      branches: [[name: '*/main']],
+      doGenerateSubmoduleConfigurations: false,
+      extensions: [],
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/librabbitmqport.git']]])
+  }
+  stage('Build') {
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/librabbitmqport.git'), string(name: 'PORT_DESCRIPTION', value: 'RabbitMQ C AMQP client library' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
+  }
+}
